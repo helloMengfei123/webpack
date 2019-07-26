fc4424459ca4d5c75d79444d31a66baf35d5fd56
mengfeii <1241461622@qq.com>
2019-07-26 10:56:36 +0800

    代码分离

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import _ from 'lodash';
-
-import { cube } from './math.js';
-function component() {
-    var element = document.createElement('pre');
-      element.innerHTML = [
-       'Hello webpack!',
-       '5 cubed is equal to ' + cube(5)
-     ].join('\n\n');
-    return element;
-  }
+   async function getComponent() {
+     var element = document.createElement('div');
+     const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
   
-   document.body.appendChild(component());
+     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+     return element;
+    }
+  
+    getComponent().then(component => {
+      document.body.appendChild(component);
+    });
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: {
-        app: './src/index.js',
-        // print: './src/print.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist',
-      hot: true
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Output Management'
-        }),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
+  entry: {
+    index: './src/index.js',
+    // another: './src/another-module.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Code Splitting'
+    }),
+
+  ],
+  //   optimization: {
+  //     splitChunks: {
+  //         cacheGroups: {
+  //             commons: {
+  //                 name: "commons",
+  //                 chunks: "initial",
+  //                 minChunks: 2
+  //             }
+  //         }
+  //     }
+  // },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
